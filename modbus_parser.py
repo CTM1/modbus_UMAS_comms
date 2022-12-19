dc14c784f63f97e5c43765d044bec3128ecb4265
latiol2600 <jerome.larvi@ecole2600.com>
2022-12-19 09:57:41 -0500

    push2 modbus parser

--- modbus_parser.py
+++ modbus_parser.py
@@ -3,20 +3,26 @@ import scapy.contrib.modbus as mb
 
 packets = rdpcap("7.download_project.pcapng")
 
+responses = []
+
 for packet in packets:
     if mb.ModbusPDUReservedFunctionCodeResponse in packet:
         data = packet.payload
+        data = str(data)
+        #print(data)
+        _, data = data.split("Z", 1)
+        responses.append(data)
 
-data = str(data)
 #print(data)
-a, data = data.split("Z")
-data = data[:-1]
 
-print(data)
+for res in responses:
+    print(res)
+
+
 
 #data = data.replace("x", '')
 #print(data)
 #data = int(data, 16)
 #print(data)
 #data = bytes.fromhex(data)
-#print(data)
+#blalblaZdd
