21f19f9497c26c2e1c001463843fdb32b81d304d
CTM1 <45026430+CTM1@users.noreply.github.com>
2022-12-21 20:21:23 +0100

    feat(parser): INIT_COMMS parsing for POC

--- parser_messages.py
+++ parser_messages.py
@@ -3,92 +3,59 @@ def parse_init_comm_res(packet):
     
     if packet[1] == 0xfe:
         print("\t[+] OK - 0xfe")
-    elif packet[1] == 0xfd:
+        if (packet[2:4] != b''):
+            print("\t[+] Frame Size: " + str(int.from_bytes(packet[2:4], 'little')))
+            print("\t[+] Data: " + str(packet[4:]))
+        else:
+            print("\t[-] PLC Reserved")
+        return
+
+    if packet[1] == 0xfd:
         print("\t[-] KO - 0xfd")
     else:
         print("\t[?] Unknown Response")
-        pass
-
+    
+    return
 
 def parse_init_comm_req(packet):
-    pass
+    return
 
 def parse_read_id_res(packet):
-    if packet[1] == 0xfe:
-        print("\t[+] OK - 0xfe")
-    elif packet[1] == 0xfd:
-        print("\t[-] KO - 0xfd")
-    else:
-        print("\t[?] Unknown Response")
-        pass
-
+    return
 
 def parse_read_id_req(packet):
-    pass
+    return
 
 def parse_read_proj_info_res(packet):
-
-    if packet[1] == 0xfe:
-        print("\t[+] OK - 0xfe")
-    elif packet[1] == 0xfd:
-        print("\t[-] KO - 0xfd")
-    else:
-        print("\t[?] Unknown Response")
-        pass
-
+    return
 
 def parse_read_proj_info_req(packet):
-    pass
+    return
 
 def parse_read_plc_info_res(packet):
-
-    if packet[1] == 0xfe:
-        print("\t[+] OK - 0xfe")
-    elif packet[1] == 0xfd:
-        print("\t[-] KO - 0xfd")
-    else:
-        print("\t[?] Unknown Response")
-        pass
-
+    return
 
 def parse_read_plc_info_req(packet):
-    pass
+    return
 
 def parse_read_card_info_res(packet):
-    if packet[1] == 0xfe:
-        print("\t[+] OK - 0xfe")
-    elif packet[1] == 0xfd:
-        print("\t[-] KO - 0xfd")
-    else:
-        print("\t[?] Unknown Response")
-        pass
+    return
 
 def parse_read_card_info_req(packet):
-    pass
+    return
 
 def parse_repeat_res(packet):
-    if packet[1] == 0xfe:
-        print("\t[+] OK - 0xfe")
-    elif packet[1] == 0xfd:
-        print("\t[-] KO - 0xfd")
-    else:
-        print("\t[?] Unknown Response")
-        pass
+    return
 
 def parse_repeat_req(packet):
-    pass
+    return
 
 def parse_take_plc_res(packet):
-    if packet[1] == 0xfe:
-        print("\t[+] OK - 0xfe")
-    elif packet[1] == 0xfd:
-        print("\t[-] KO - 0xfd")
-    else:
-        print("\t[?] Unknown Response")
-        pass
+    return
+
     
 def parse_take_plc_req(packet):
-    pass
+    return
 
 def parse_release_plc_res(packet):
     if packet[1] == 0xfe:
@@ -97,19 +64,13 @@ def parse_release_plc_res(packet):
         print("\t[-] KO - 0xfd")
     else:
         print("\t[?] Unknown Response")
-        pass
+        return
     
 def parse_release_plc_req(packet):
-    pass
+    return
 
 def parse_keep_alive_res(packet):
-    if packet[1] == 0xfe:
-        print("\t[+] OK - 0xfe")
-    elif packet[1] == 0xfd:
-        print("\t[-] KO - 0xfd")
-    else:
-        print("\t[?] Unknown Response - skipping")
-        pass
+    return
 
 def parse_keep_alive_req(packet):
-    pass
+    return
--- parser_umas.py
+++ parser_umas.py
@@ -4,26 +4,26 @@ import parser_messages
 
 res_parse_dict = {
     "1" : parser_messages.parse_init_comm_res,
-    "2" : parser_messages.parse_read_id_res,
-    "3" : parser_messages.parse_read_proj_info_res,
-    "4" : parser_messages.parse_read_plc_info_res,
-    "6" : parser_messages.parse_read_card_info_res,
-    "A" : parser_messages.parse_repeat_res,
-    "10": parser_messages.parse_take_plc_res,
-    "11": parser_messages.parse_release_plc_res,
-    "12": parser_messages.parse_keep_alive_res,
+#    "2" : parser_messages.parse_read_id_res,
+#    "3" : parser_messages.parse_read_proj_info_res,
+#    "4" : parser_messages.parse_read_plc_info_res,
+#    "6" : parser_messages.parse_read_card_info_res,
+#    "A" : parser_messages.parse_repeat_res,
+#    "10": parser_messages.parse_take_plc_res,
+#    "11": parser_messages.parse_release_plc_res,
+#    "12": parser_messages.parse_keep_alive_res,
 }
 
 req_parse_dict = {
     "1": parser_messages.parse_init_comm_req,
-    "2": parser_messages.parse_read_id_req,
-    "3": parser_messages.parse_read_proj_info_req,
-    "4": parser_messages.parse_read_plc_info_req,
-    "6": parser_messages.parse_read_card_info_req,
-    "A": parser_messages.parse_repeat_req,
-    "10": parser_messages.parse_take_plc_req,
-    "11": parser_messages.parse_release_plc_req,
-    "12": parser_messages.parse_keep_alive_req,
+#    "2": parser_messages.parse_read_id_req,
+#    "3": parser_messages.parse_read_proj_info_req,
+#    "4": parser_messages.parse_read_plc_info_req,
+#    "6": parser_messages.parse_read_card_info_req,
+#    "A": parser_messages.parse_repeat_req,
+#    "10": parser_messages.parse_take_plc_req,
+#    "11": parser_messages.parse_release_plc_req,
+#    "12": parser_messages.parse_keep_alive_req,
 }
 
 def parse_pcap(filepath: str):
@@ -40,7 +40,7 @@ def parse_pcap(filepath: str):
             umas_packet = bytes(packet.payload).split(b'Z')[1]
             if "Failed" == previous_request:
                 print("[-] Failed to identify request")
-            #                    r ID            r Packet
+            #                    ID               Packet
             responses.append([previous_request, umas_packet])
 
         if mb.ModbusPDUReservedFunctionCodeRequest in packet:
@@ -61,7 +61,7 @@ def parse_pcap(filepath: str):
 def print_responses(responses):
     for r in responses:
         try: 
-        #             r ID   r Packet
+        #                  ID   Packet
             res_parse_dict[r[0]](r[1])
         except KeyError:
             print("Request " + str(r[0]) + " not implemented.")
